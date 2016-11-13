3fbd8e531cf5ef41d87c698726497ba8677c168d
Ishmeet Kaur <ishmeet15042@iiitd.ac.in>
2016-11-13 17:05:21 +0530

    Add files via upload

---
+++ Supervisor.java
@@ -0,0 +1,35 @@
+class Supervisor
+{
+	public void addStaff(String str)
+	{
+		
+	}
+	public void viewStaff(String str)
+	{
+		
+	}
+	public void deleteStaff(String str)
+	{
+		
+	}
+	public void AssignTaskStaff(Staff s)
+	{
+		
+	}
+	public void logistics()
+	{
+		
+	}
+	public void ApproveLogistics(Staff s)
+	{
+		
+	}
+	public void viewTaskReport(Task t)
+	{
+		
+	}
+	public void sendLeve()
+	{
+		
+	}
+}
