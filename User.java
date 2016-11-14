33b3e88a1abb2f54280af25d25fb04723f86e0b0
Ishmeet Kaur <ishmeet15042@iiitd.ac.in>
2016-11-14 17:48:48 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,43 @@
+class User
+{
+	String Username;
+	String user_id;
+	String password;
+	
+	public void login(String str)
+	{
+		
+	}
+	public void logout()
+	{
+		
+	}
+	public String getUsername()
+	{
+		
+	}
+	public void setUSername()
+	{
+		
+	}
+	public String getPassword()
+	{
+		
+	}
+	public void setPassword()
+	{
+		
+	}
+	public String getUserId()
+	{
+		
+	}
+	public void setUserId()
+	{
+		
+	}
+	User()
+	{
+		
+	}
+}
